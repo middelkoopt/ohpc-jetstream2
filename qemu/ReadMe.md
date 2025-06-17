02f805869c7a16ef5f6e4829074ddceddef59ef0
Timothy Middelkoop <tmiddelkoop@internet2.edu>
2025-06-16 22:17:53 -0500

    Add ARM support

---
+++ qemu/ReadMe.md
@@ -0,0 +1,7 @@
+# QEMU Cluster
+
+## Build packages
+
+```bash
+apt-get install --yes podman qemu-utils qemu-system-x86 qemu-system-arm ovmf dosfstools genisoimage 
+```
--- qemu/build.sh
+++ qemu/build.sh
@@ -78,8 +78,9 @@ EOF
 genisoimage -output seed.img -volid cidata -rational-rock -joliet -input-charset utf-8 cloud-init
 
 echo "--- run qemu"
-qemu-system-x86_64 -accel kvm -cpu host -m 2G \
-    -bios /usr/share/ovmf/OVMF.fd \
+declare -A efi_bios=(["x86_64"]="/usr/share/ovmf/OVMF.fd" ["aarch64"]="/usr/share/qemu-efi-aarch64/QEMU_EFI.fd")
+qemu-system-$HOSTTYPE -accel kvm -machine virt -cpu host -m 2G \
+    -bios ${efi_bios[$HOSTTYPE]} \
     -drive if=virtio,file=drive.qcow2,format=qcow2 \
     -drive if=virtio,file=seed.img,format=raw,media=cdrom \
     -nic user,model=virtio-net-pci,hostfwd=tcp::8022-:22 \
