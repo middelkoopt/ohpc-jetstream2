2e9f157112e0f6ab699ea66ad3cb6a70adc046a8
Timothy Middelkoop <tmiddelkoop@internet2.edu>
2025-11-26 19:23:05 -0600

    need more memory!
    Signed-off-by: Timothy Middelkoop <tmiddelkoop@internet2.edu>

--- qemu/create.sh
+++ qemu/create.sh
@@ -12,7 +12,7 @@ echo '--- remove old ssh key'
 ssh-keygen -R "[localhost]:8022"
 
 ./generate-seed.sh
-./run-image.sh head 2 2
+./run-image.sh head 4 2
 
 for (( i=1 ; i<=n ; i++ )) ; do
   ./run-image.sh c$i $ram $cpus
